c9adb7b15df75cdf46d4118ce18403b5e085693d
Diseyi <filomiinaa@gmail.com>
2023-02-22 12:52:10 +0100

    feat: update comments from local storage

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const videotime = document.getElementById('videotime');
 const div = document.getElementById('notes');
 const playspan = document.getElementById('playspan');
 const pausespan = document.getElementById('pausespan');
-const timestampspan = document.querySelectorAll('.timestamp');
+const timestampspan = document.querySelectorAll('li');
+
+let commentlist;
+let videoname;
+let savedNotes = {}
 
 const secondsToMinute = (time) => {
     const seconds = time;
@@ -27,10 +31,13 @@ const secondsToMinute = (time) => {
 const getVideo = () => {
     const videofile = video.files[0]
     filename.innerText = videofile.name
+    videoname = videofile.name
     const videoURL = URL.createObjectURL(videofile);
     videoplayer.style.display = "block"
     videoplayer.removeAttribute('controls')
     videoplayer.src = videoURL
+
+    getFromLocalStorage()
 }
 
 const playVideo = () => {
@@ -82,22 +89,71 @@ const showInputBox = () => {
     openInputButton.style.display = "none";
 }
 
+const gotoTimestamp = (e) => {
+    console.log(e)
+}
+
 const saveToLocalStorage = (title, value) => {
     localStorage.setItem(title, value)
 }
+
 const getFromLocalStorage = () => {
     const local = { ...localStorage }
     for (const key in local) {
-        const p = document.createElement('p')
-        p.innerText = `${key} : ${local[key]}`
+        if (key === videoname) {
+            savedNotes = JSON.parse(localStorage.getItem(key))
+            break;
+        }
+    }
+
+    for (const key in savedNotes) {
+        const p = document.createElement('li')
+        const timestamp = document.createElement('span')
+        const comment = document.createElement('span')
+
+        timestamp.setAttribute('class', 'timestamp')
+        timestamp.innerText = `${key}`
+        comment.innerText = `: ${savedNotes[key]}`
+
+        p.appendChild(timestamp)
+        p.appendChild(comment)
         div.appendChild(p)
     }
+    commentlist = document.querySelectorAll('.timestamp');
+
+    if (commentlist && commentlist.length > 0) {
+        for (let i = 0; i < commentlist.length; i++) {
+            commentlist[i].addEventListener('click', (e) => {
+                const text = commentlist[i].textContent;
+                const timeArray = text.split(":")
+                let totalSeconds = 0;
+
+                for (let i = 0; i < timeArray.length; i++) {
+                    let value = parseInt(timeArray[i], 10);
+                    if (!isNaN(value)) return;
+                    if (i === 0) {
+                        totalSeconds += value;
+                    }
+                    if (i === 1) {
+                        totalSeconds += value;
+                    }
+                    if (i === 2) {
+                        totalSeconds += value * 60;
+                    }
+                }
+                videoplayer.currentTime = totalSeconds
+            })
+        }
+    }
+
 }
+
 const closeEvents = () => {
     input.style.display = "none";
     closeInputButton.style.display = "none";
     openInputButton.style.display = "block";
 }
+
 const closeInputBox = () => {
     if (input.value === '') {
         alert('no comment was added')
@@ -105,7 +161,7 @@ const closeInputBox = () => {
         return;
     }
     const value = input.value
-    const p = document.createElement('p')
+    const p = document.createElement('li')
     const timestamp = document.createElement('span')
     const comment = document.createElement('span')
 
@@ -116,22 +172,18 @@ const closeInputBox = () => {
 
     timestamp.innerText = `${time}`
     comment.innerText = `: ${value}`
+
+    savedNotes[time] = value
+
     p.appendChild(timestamp)
     p.appendChild(comment)
 
     div.appendChild(p)
     input.value = ""
-    saveToLocalStorage(time, value)
+    saveToLocalStorage(videoname, JSON.stringify(savedNotes))
     closeEvents()
 }
 
-const gotoTimestamp = () => {
-    console.log('heelo')
-}
-
-for (let i = 0; i < timestampspan.length; i++) {
-    timestampspan[i].addEventListener('click', gotoTimestamp)
-}
 
 video.addEventListener('change', getVideo)
 play.addEventListener('click', playVideo)
